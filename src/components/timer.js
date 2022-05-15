30cabfb8d3c1ddfa0c6ea77c64b4b436abcc4ee7
JonasFiechter <jonasrfiechter@gmail.com>
2022-05-15 16:25:30 -0300

    update
    start and pause btns working

--- src/components/timer.js
+++ src/components/timer.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {View,StyleSheet} from 'react-native';
 import { Countdown } from './countdown.js'
 import { my_colors } from '../utils/colors';
+import { RoundedButton } from './roundedButton.js';
 
-
-export const Timer = ({focusSubject}) => (
-    <View style={styles.countdownContainer}>
-        <View style={styles.countdown}>
-            <Countdown onProgress={() => {}} onEnd={() => {}}/>
+export const Timer = ({focusSubject}) => {
+    const [isStarted, setIsStarted] = useState(false)
+    return (
+        <View style={styles.countdownContainer}>
+            <View style={styles.countdown}>
+                <Countdown isPaused={!isStarted} onProgress={() => {}} onEnd={() => {}}/>
+            </View>
+            <View style={styles.buttonWrapper}>
+                {!isStarted && 
+                    <RoundedButton title='Start' onPress={()=>{setIsStarted(true)}}>
+                    </RoundedButton>}
+                {isStarted && 
+                <RoundedButton title='Pause' onPress={()=>{setIsStarted(false)}}>
+                </RoundedButton>}
+            </View>
         </View>
-    </View>
-)
+    );
+};
 
 const styles = StyleSheet.create ({
     countdownContainer: {
         flex: 1,
-        backgroundColor: 'gray',
     },
     countdown: {
         flex: 0.5,
-        backgroundColor: 'yellow',
         alignItems: 'center',
         justifyContent: 'center',
     },
+    buttonWrapper: {
+        flex: 0.3,
+        flexDirection: 'row',
+        padding: 15,
+        justifyContent: 'center',
+        alignContent: 'center',
+    }
 })
